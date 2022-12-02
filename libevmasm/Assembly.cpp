51398ed5559b5fe96280df8b2233e80e38175657
Alex Beregszaszi <alex@rtfs.hu>
2022-12-03 00:38:30 +0100

    Remove typecast (size is determined by output)

--- libevmasm/Assembly.cpp
+++ libevmasm/Assembly.cpp
@@ -1076,7 +1076,7 @@ LinkerObject const& Assembly::assemble(bool eof) const
 	if (eof)
 	{
 		assertThrow(codeLength > 0 && codeLength <= 0xffff, AssemblyException, "Invalid code section size.");
-		toBigEndian(uint16_t(codeLength), eofCodeLength);
+		toBigEndian(codeLength, eofCodeLength);
 	}
 
 	auto const dataStart = ret.bytecode.size();
@@ -1161,7 +1161,7 @@ LinkerObject const& Assembly::assemble(bool eof) const
 	if (eof)
 	{
 		assertThrow(/*dataLength >= 0 && */ dataLength <= 0xffff, AssemblyException, "Invalid data section size.");
-		toBigEndian(uint16_t(dataLength), eofDataLength);
+		toBigEndian(dataLength, eofDataLength);
 	}
 
 	return ret;
