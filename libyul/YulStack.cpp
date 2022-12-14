bb067d38de2ef3d9800cd847cdeb07ab4c733712
Daniel Kirchner <daniel@ekpyron.org>
2022-12-14 12:06:27 +0100

    Initial still broken version.

--- libyul/YulStack.cpp
+++ libyul/YulStack.cpp
@@ -234,6 +234,7 @@ void YulStack::optimize(Object& _object, bool _isCreation)
 
 	OptimiserSuite::run(
 		dialect,
+		m_eofVersion,
 		meter.get(),
 		_object,
 		// Defaults are the minimum necessary to avoid running into "Stack too deep" constantly.
