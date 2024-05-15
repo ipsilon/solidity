551df6cf64a0c64b6690a21d8f91cdcbef74ceaa
pdobacz <5735525+pdobacz@users.noreply.github.com>
2024-05-15 10:33:15 +0200

    Clear output_data on EOFCREATE
    (cherry picked from commit 7d22f237d7a26fecead96cf15d5cf1d026c3f5d7)

--- test/EVMHost.cpp
+++ test/EVMHost.cpp
@@ -430,7 +430,10 @@ evmc::Result EVMHost::call(evmc_message const& _message) noexcept
 	if (result.status_code != EVMC_SUCCESS)
 		accounts = stateBackup;
 
-	return result;
+	if (message.depth > 0 && message.kind == EVMC_EOFCREATE)
+		return evmc::Result{result.status_code, result.gas_left, result.gas_refund, message.recipient};
+	else
+	    return result;
 }
 
 evmc::bytes32 EVMHost::get_block_hash(int64_t _number) const noexcept
