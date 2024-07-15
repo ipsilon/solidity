52cc6eb3c2f18befbb0056e54c20595df48af772
rodiazet <radek.zagorowicz@gmail.com>
2024-07-15 14:23:12 +0200

    eof: call -> extcall

--- test/libsolidity/semanticTests/reverts/revert_return_area.sol
+++ test/libsolidity/semanticTests/reverts/revert_return_area.sol
@@ -7,7 +7,8 @@ contract C {
         address x = address(this);
         assembly {
             mstore(0, 7)
-            s := call(sub(0, 1), x, 0, 0, 0, 0, 32)
+            s := extcall(x, 0, 0, 0)
+            returndatacopy(0, 0, 32)
             r := mload(0)
         }
     }
@@ -15,4 +16,4 @@ contract C {
 // ====
 // EVMVersion: >=byzantium
 // ----
-// f() -> 0x00, 0x08c379a000000000000000000000000000000000000000000000000000000000
+// f() -> 0x01, 0x08c379a000000000000000000000000000000000000000000000000000000000
