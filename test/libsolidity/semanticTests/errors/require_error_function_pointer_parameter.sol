1cb17b82304254d1ca4bd75c352c1839bc3a428d
rodiazet <radek.zagorowicz@gmail.com>
2024-07-16 16:36:03 +0200

    eof: WIP in tests. Make results compatible for non-optimised

--- test/libsolidity/semanticTests/errors/require_error_function_pointer_parameter.sol
+++ test/libsolidity/semanticTests/errors/require_error_function_pointer_parameter.sol
@@ -15,4 +15,4 @@ contract C
 }
 
 // ----
-// f() -> FAILURE, hex"271b1dfa", hex"7f5417cf91e6e2618deffc9ca4266c509783dd54f37cdc8e0000000000000000"
+// f() -> FAILURE, hex"271b1dfa", hex"8ff77dee959af275e2a83aeb93366e425c1abf0ff37cdc8e0000000000000000"
